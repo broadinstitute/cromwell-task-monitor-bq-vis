fae4eb0516b005e49c7e16b8a67ccac527799f60
bshifaw <bshifaw@broadinstitute.com>
2024-02-09 11:49:43 -0500

    Added logging folder to for exceptions

---
+++ src/cromwellMonitor/logging/logging.py
@@ -0,0 +1,18 @@
+import logging
+
+
+def handle_value_error(err):
+    logging.error(f"A ValueError occurred: {err}")
+
+
+def handle_type_error(err):
+    logging.error(f"A TypeError occurred: {err}")
+
+
+def handle_runtime_error(err):
+    logging.error(f"A RuntimeError occurred: {err}")
+
+
+def handle_firecloud_server_error(err, message):
+    logging.error(f"FISS ERROR: {message}: {err}")
+
