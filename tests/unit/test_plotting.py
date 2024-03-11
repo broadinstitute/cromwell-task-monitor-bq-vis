136d9b3e3c294671a0cc918e780d46b2c8d52d2f
bshifaw <bshifaw@broadinstitute.org>
2024-03-11 14:23:39 -0400

    Refactor task detailed plot (#5)
    * returning fig instead of matplot object for plot_detailed_resource_usage
    
    * made plot_detailed_resource_usage more modular
    
    * created function get_unique_shards_for_task
    
    * minor fix to cpu task detailed legend plot, added unit test for subplot_resource_usage
    
    ---------
    
    Co-authored-by: bshifaw <bshifaw@broadinstitute.com>

--- tests/unit/test_plotting.py
+++ tests/unit/test_plotting.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import pytest
+from matplotlib import pyplot as plt
 
 from cromonitor.plotting import plotting
 
@@ -37,3 +39,48 @@ class TestPlotting:
         # Assert that the result is as expected
         pd.testing.assert_frame_equal(task_summary_df, expected_df)
         assert duration_sum == {"write_to_stdout": 171}
+
+    @pytest.fixture
+    def subplot(self):
+        fig, ax = plt.subplots()
+        return ax
+
+    @pytest.fixture
+    def df_monitoring_task_shard(self):
+        return pd.DataFrame(
+            {
+                "metrics_timestamp": pd.date_range(start="1/1/2021", periods=5),
+            }
+        )
+
+    @pytest.fixture
+    def resource_used_array(self):
+        return [10, 20, 30, 40, 50]
+
+    @pytest.fixture
+    def runtime_dic(self):
+        return {
+            "available_cpu_cores": 4,
+            "available_mem_gb": 16,
+            "available_disk_gb": 100,
+            "requested_cpu_cores": 2,
+            "requested_mem_gb": 8,
+            "requested_disk_gb": 50,
+        }
+
+    def test_subplot_resource_usage(
+        self, subplot, df_monitoring_task_shard, resource_used_array, runtime_dic
+    ):
+        result = plotting.subplot_resource_usage(
+            subplot=subplot,
+            df_monitoring_task_shard=df_monitoring_task_shard,
+            resource_used_array=resource_used_array,
+            runtime_dic=runtime_dic,
+            task_shard_duration=100,
+            resource_label="CPU",
+            y_label="CPU % Used",
+            obtained_resource_key="available_cpu_cores",
+            requested_resource_key="requested_cpu_cores",
+            available_resource=4.0,
+        )
+        assert isinstance(result, plt.Axes)
