ad42a4f2e31741ba8f0e9613cdae442b43146096
bshifaw <bshifaw@broadinstitute.com>
2024-03-14 10:02:54 -0400

    Added test for getting end date

--- tests/unit/test_fiss_utils.py
+++ tests/unit/test_fiss_utils.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
-
+from unittest.mock import patch
 import pandas as pd
 import pytest
 
 from cromonitor.fiss import utils
+from cromonitor.fiss.utils import Workflow
 
 
 class TestFissUtils:
@@ -49,3 +50,45 @@ class TestFissUtils:
 
         # Assert
         assert result == 5
+
+    @patch('cromonitor.fiss.utils.get_workflow_metadata_api_call')
+    @pytest.mark.parametrize(
+        "days, expected",
+        [
+            (0, -7),
+            (1, -6),
+        ],
+    )
+    def test_get_query_end_time(self, mock_get_workflow_metadata_api_call, days,
+                                expected):
+        class MockResponse:
+            def __init__(self, status_code, json_data):
+                self.status_code = status_code
+                self.json_data = json_data
+
+            def json(self):
+                return self.json_data
+
+        # Date
+        # Subtract 14 days from the current date and time
+        date_14_days_ago = datetime.now() - timedelta(days=days)
+
+        # Format the date in the desired format
+        formatted_date = date_14_days_ago.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+
+        mock_get_workflow_metadata_api_call.return_value = MockResponse(
+            status_code=200,
+            json_data={"start": f"{formatted_date}", "end": None}
+        )
+        workflow = Workflow(
+            workspace_namespace="namespace",
+            workspace_name="workspace",
+            submission_id="submission_id",
+            parent_workflow_id="workflow_id"
+        )
+
+        # Act
+        result = workflow._get_query_end_time()
+
+        # Assert
+        assert result == expected
