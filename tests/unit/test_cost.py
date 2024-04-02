2c2e3b27b48c9392e562a37b2c7de6900fdac017
bshifaw <bshifaw@broadinstitute.com>
2024-04-02 16:53:05 -0400

    one unit test

---
+++ tests/unit/test_cost.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+from unittest.mock import patch
+
+import pandas as pd
+import pytest
+
+from cromonitor.query import cost
+
+
+class TestCost:
+    @pytest.mark.parametrize(
+        "end_time, expected",
+        [
+            (datetime.now() - timedelta(hours=23), False),
+            (datetime.now() - timedelta(hours=25), True),
+        ],
+    )
+    def test_check_minimum_time_passed_since_workflow_completion(
+            self,
+            end_time: datetime,
+            expected: bool,
+    ):
+        # Act
+        result = cost.check_minimum_time_passed_since_workflow_completion(end_time)
+
+        # Assert
+        assert result[0] == expected
+
