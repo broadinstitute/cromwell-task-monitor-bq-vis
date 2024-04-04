e3b5a3d00af59ada945224c7f9c77345dffa4fcc
bshifaw <bshifaw@broadinstitute.com>
2024-04-04 15:23:50 -0400

    threshold option to cost plot and order the task using cost

--- src/cromonitor/plotting/cost_plots.py
+++ src/cromonitor/plotting/cost_plots.py
@@ -13,15 +13,15 @@ class CostPlots:
     """
 
     def __init__(self, cost_data: pd.DataFrame):
-
         self.cost_data: pd.DataFrame = cost_data
         self.cost_data.fillna("NA", inplace=True)  # Fill NaN values with 'NA'
 
     def plot_workflow_cost(
-        self,
-        title: Optional[str] = "Workflow Cost Per Task",
-        group_by_description: Optional[bool] = False,
-        color: Optional[str] = "cost_description",
+            self,
+            title: Optional[str] = "Workflow Cost Per Task",
+            group_by_description: Optional[bool] = False,
+            color: Optional[str] = "cost_description",
+            threshold_cost_percent: Optional[float] = None,
     ) -> px.bar:
         """
         Plot the cost data
@@ -30,19 +30,26 @@ class CostPlots:
         :param color: Color the bars by cost description
         or specify a column to color by.
         If set to None, no coloring will be done.
+        :param threshold_cost_percent: Optional (float between 0 and 100).
+        Show only entries with a cost greater than or equal to the specified percentage
+        of the total cost.
         :return: A plotly Figure object with the cost data plot.
         """
 
         return plotly_bar_cost(
-            self.cost_data, title=title, group_des=group_by_description, color=color
+            self.cost_data,
+            title=title,
+            group_des=group_by_description,
+            color=color,
+            threshold_cost_percent=threshold_cost_percent
         )
 
     def plot_task_cost(
-        self,
-        task_name: str,
-        title: Optional[str] = "Task Cost Per Workflow",
-        group_by_description: Optional[bool] = False,
-        color: Optional[str] = "cost_description",
+            self,
+            task_name: str,
+            title: Optional[str] = "Task Cost Per Workflow",
+            group_by_description: Optional[bool] = False,
+            color: Optional[str] = "cost_description",
     ) -> Union[px.bar, None]:
         """
         Plot the cost data
@@ -72,27 +79,48 @@ class CostPlots:
 
 
 def plotly_bar_cost(
-    cost_df,
-    title,
-    group_des: Optional[bool] = False,
-    color: Optional[str] = None,
+        cost_df,
+        title,
+        group_des: Optional[bool] = False,
+        color: Optional[str] = None,
+        threshold_cost_percent: Optional[float] = None,
 ) -> px.bar:
     """
     Plot the cost data
+
     :param cost_df: DataFrame with cost data
     :param title: Title for the plot
     :param group_des: Group the cost by description
     :param color: Color the bars by specified column
+    :param threshold_cost_percent: Optional (float between 0 and 100).
+    Show only entries with a cost greater than or equal to the specified percentage of
+    the total cost.
     :return: A plotly Figure object with the cost data plot.
     """
 
+    if threshold_cost_percent:
+        cost_df_sorted = filter_dataframe_by_cost_threshold(
+            dataframe=cost_df, threshold_percent=threshold_cost_percent
+        )
+        # Sort the DataFrame by 'cost' in descending order
+        cost_df_sorted = cost_df_sorted.sort_values(by=['cost'], ascending=False)
+    else:
+        # Sort the DataFrame by 'cost' in descending order
+        cost_df_sorted = cost_df.sort_values(by=['cost'], ascending=False)
+
+    # Group by 'task_name', calculate the sum of 'cost' for each task, sort in
+    # descending order. Used to order the x-axis based on the total cost of each task.
+    task_order = cost_df_sorted.groupby('task_name')['cost'].sum().sort_values(
+        ascending=False).index
+
     fig = px.bar(
-        cost_df,
+        cost_df_sorted,
         x="task_name",
         y="cost",
         color=color,
         title=title,
         hover_data="machine_spec",
+        category_orders={"task_name": task_order}
     )
 
     fig.update_layout(
@@ -108,3 +136,51 @@ def plotly_bar_cost(
         )
 
     return fig
+
+
+def filter_dataframe_by_cost_threshold(
+        dataframe, threshold_percent, task_column='task_name', cost_column='cost'
+) -> pd.DataFrame:
+    """
+    Apply a threshold to the DataFrame based on the total cost of the tasks.
+    :param dataframe: Dataframe to apply threshold to
+    :param threshold_percent: threshold percentage to apply
+    :param task_column: The task column name
+    :param cost_column: The cost column name
+    :return: The filtered DataFrame
+    """
+
+    if not 0 <= threshold_percent <= 100:
+        log.handle_user_error(
+            err=None, message="Threshold percentage must be between 0 and 100."
+        )
+
+    grouped_and_sorted_df = dataframe.groupby(task_column)[
+        cost_column].sum().sort_values(ascending=False)
+
+    total_cost = dataframe[cost_column].sum()
+    cost_threshold = total_cost * (threshold_percent / 100)
+
+    filtered_dataframe = pd.DataFrame()
+    for task, cost in grouped_and_sorted_df.items():  # Iterate over the task names starting from the most expensive
+        # Filter the dataframe for the current task
+        task_df = dataframe[dataframe[task_column] == task]
+
+        # If the filtered_dataframe is empty, this is the first iteration
+        # So; we add the current task's dataframe to the filtered_dataframe
+        if filtered_dataframe.empty:
+            filtered_dataframe = pd.concat([filtered_dataframe, task_df])
+        else:
+            # If the filtered_dataframe is not empty, we check if the total cost of
+            # the current filtered_dataframe and the current task's dataframe is less
+            # than or equal to the cost threshold If it is, we add the current task's
+            # dataframe to the filtered_dataframe
+            if pd.concat([filtered_dataframe, task_df])[
+                cost_column].sum() <= cost_threshold:
+                filtered_dataframe = pd.concat([filtered_dataframe, task_df])
+            else:
+                # If the total cost is greater than the cost threshold, we break the
+                #  loop; this means we stop adding more tasks to the filtered_dataframe
+                break
+
+    return filtered_dataframe
---
+++ tests/unit/test_cost_plots.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+from src.cromonitor.plotting.cost_plots import filter_dataframe_by_cost_threshold
+
+def test_filter_dataframe_by_cost_threshold():
+    # Create a test DataFrame
+    data = {
+        'task_name': ['task1', 'task2', 'task3', 'task4'],
+        'cost': [100, 200, 300, 400]
+    }
+    df = pd.DataFrame(data)
+
+    # Call the function with the test DataFrame and a threshold percentage
+    threshold_percent = 50  # 50%
+    filtered_df = filter_dataframe_by_cost_threshold(df, threshold_percent)
+
+    # Calculate the total cost in the original DataFrame
+    total_cost = df['cost'].sum()
+
+    # Calculate the total cost in the filtered DataFrame
+    filtered_total_cost = filtered_df['cost'].sum()
+
+    # Assert that the total cost in the filtered DataFrame is less than or equal to the threshold percentage
+    assert filtered_total_cost <= total_cost * (threshold_percent / 100)
+
+    # Assert that the tasks in the filtered DataFrame are the ones with the highest cost
+    assert set(filtered_df['task_name']) == set(['task4'])
